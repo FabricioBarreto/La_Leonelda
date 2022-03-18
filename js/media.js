9c12ff7df91388394528962cccf2ea1ef5cf8025
fabrib26 <fabrib40@gmail.com>
2022-03-18 16:56:59 -0300

    lastet commit

--- js/media.js
+++ js/media.js
@@ -1,12 +1,14 @@
-<<<<<<< HEAD
 var btn = document.getElementById("btn-menu");
-const header = document.querySelector(".links-header")
+const linksHeader = document.querySelector(".links-header");
 
-btn.addEventListener("click", function() {
-   header.classList.toggle("visible")
+btn.addEventListener("click", e =>{
+   header.classList.toggle("visible");
 });
-=======
-var btn = document.querySelector("btn_menu");
 
-btn.addEventListener('click', "btn_menu" )
->>>>>>> af43a4f557a58700c7a0b13b5aa585e139076f68
+var btnLengauge = document.querySelector(".btn-language");
+const menuLenguage = document.querySelector(".menu-lenguage");
+
+btnLengauge.addEventListener("click", function(){
+   // menuLenguage.classList.toggle("show-lenguage");
+  alert("hola");
+});
